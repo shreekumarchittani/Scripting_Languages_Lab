1062dd33251676afa24a8ba45cfb335079a16d75
Shreekumar hegde <43261685+shreekumarchittani@users.noreply.github.com>
2019-09-16 16:24:12 +0530

    Add files via upload

---
+++ LongestWord/FindingLongWord.js
@@ -0,0 +1,16 @@
+console.log("hello")
+function FindMax(){
+	var Mystring = document.getElementById("str").value;
+	var arr = Mystring.split(" ");
+	var i;
+	var maxlen;
+	maxlen=-1;
+	var maxString;
+	for(i=0;i<arr.length;i++){
+		if(arr[i].length>maxlen){
+		 	maxlen = arr[i].length;
+		 	maxString = arr[i];	
+		 }
+		 }
+	document.getElementById("res").innerHTML = maxString;
+}
