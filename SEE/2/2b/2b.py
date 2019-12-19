df3c82053deabe0b8a4973b53b0c8afe66200850
Shreekumar hegde <43261685+shreekumarchittani@users.noreply.github.com>
2019-12-19 21:30:31 +0530

    Update 2b.py

--- SEE/2/2b/2b.py
+++ SEE/2/2b/2b.py
@@ -3,10 +3,10 @@ from pandas import Series,DataFrame
 import matplotlib.pyplot as plt
 import seaborn as sns
 df = pd.read_csv('student.csv')
-'''print(df.head)
+print(df.head)
 print(df.describe())
 df = df.drop(['lunch','test preparation course'],axis=1)
-print(df)'''
+print(df)
 df['parental level of education'] = df['parental level of education'].fillna('Null')
 print(df)
 ax = sns.countplot(x='test preparation course',hue='gender',data=df)
